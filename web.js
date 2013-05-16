b75be24abd57b0f730c717f694175c6b5b26bb4d
Julien Fusco <pkoin.koin@gmail.com>
2013-05-17 00:08:40 +0200

    Mod: build app and use it on heroku Yipi o/

--- web.js
+++ web.js
@@ -2,6 +2,5 @@ var gzippo = require('gzippo');
 var express = require('express');
 
 var app = express.createServer(express.logger());
-app.use(gzippo.staticGzip(__dirname + '/app'));
+app.use(gzippo.staticGzip(__dirname + '/dist'));
 app.listen(process.env.PORT || 5000);
-
