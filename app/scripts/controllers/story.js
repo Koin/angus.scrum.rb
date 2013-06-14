6997bd3b6f6f51e33e41a63f1613a028b895709a
Julien Fusco <pkoin.koin@gmail.com>
2013-06-14 23:41:45 +0200

    Add: stories can be ordered

--- app/scripts/controllers/story.js
+++ app/scripts/controllers/story.js
@@ -1,17 +1,17 @@
 'use strict';
 
 angular.module('angus.scrum.rbApp')
-  .controller('StoryCtrl', function ($scope, $routeParams, Products, Stories) {
+  .controller('StoryCtrl', function ($scope, $rootScope, $routeParams, Products, Stories) {
 
-    $scope.product = Products.get({id:$routeParams.productId});
-
-    $scope.stories = Stories.query();
+    $scope.productId = $routeParams.productId;
+    $scope.stories = Stories.query({product_id:$scope.productId});
 
     $scope.edit = function(story) {
       $scope.story = story;
     };
 
     $scope.save = function() {
+      $scope.story.product_id = $scope.productId;
       if($scope.story.$save) {
         $scope.story.$save();
       } else {
@@ -24,5 +24,25 @@ angular.module('angus.scrum.rbApp')
       $scope.stories.splice($index, 1);
       story.$destroy();
     };
+
+    $rootScope.$on('dropEvent', function(evt, dragged, dropped) {
+      var i, oldIndex1, oldIndex2;
+      for(i=0; i<$scope.stories.length; i++) {
+        var c = $scope.stories[i];
+        if(dragged.id === c.id) {
+          oldIndex1 = i;
+        }
+        if(dropped.id === c.id) {
+          oldIndex2 = i;
+        }
+      }
+      var temp = $scope.stories[oldIndex1];
+      $scope.stories[oldIndex1] = $scope.stories[oldIndex2];
+      $scope.stories[oldIndex2] = temp;
+      $scope.stories[oldIndex1].order = oldIndex1;
+      $scope.stories[oldIndex1].$save();
+      $scope.stories[oldIndex2].order = oldIndex2;
+      $scope.stories[oldIndex2].$save();
+    });
 
   });
