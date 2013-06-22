ba57f58277a63ff28d92b4c10956a07d23137571
Julien Fusco <pkoin.koin@gmail.com>
2013-06-22 23:55:58 +0200

    Add: tasks

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,6 +13,10 @@ angular.module('angus.scrum.rbApp', ['ngResource'])
         templateUrl: '/views/stories.html',
         controller: 'StoryCtrl'
       })
+      .when('/product/:productId/story/:storyId/tasks.html', {
+        templateUrl: '/views/tasks.html',
+        controller: 'TaskCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
@@ -30,6 +34,16 @@ angular.module('angus.scrum.rbApp', ['ngResource'])
   })
   .factory('Stories', function($resource) {
     return $resource('http://' + apiServer + '/stories/:id', {
+      id: '@id'
+    }, {
+      query :   { method : 'GET', isArray : true },
+      save :    { method : 'PUT' },
+      create :  { method : 'POST' },
+      destroy : { method : 'DELETE' }
+    });
+  })
+  .factory('Tasks', function($resource) {
+    return $resource('http://' + apiServer + '/tasks/:id', {
       id: '@id'
     }, {
       query :   { method : 'GET', isArray : true },
